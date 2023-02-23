0f57185658a40945688466245531e739296363b3
Felipe F. Porto <106607666+felipefporto@users.noreply.github.com>
2023-02-23 19:51:52 -0300

    Add files via upload

---
+++ Exemplo_aula_1_2023_02_23/Exemplo_aula_1_2023_02_23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_aula_1_2023_02_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!\n");
+
+            int a, b, total;
+            a = 20;
+            b = 10;
+
+            Console.WriteLine(a + " + " + b + " = " + (a + b));
+            total = a - b;
+            Console.WriteLine(a + "-" + b + "=" + total);
+            Console.WriteLine(a + "*" + b + "=" + (a * b));
+            Console.WriteLine(a + " / " + b + " = " + (a / b));
+            Console.WriteLine(a + "%" + b + "=" + (a % b));
+
+            Console.ReadKey();
+
+        }
+    }
+}
