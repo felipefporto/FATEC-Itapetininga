3d17427bb4112eb07f840df235cca495043fac61
Felipe F. Porto <106607666+felipefporto@users.noreply.github.com>
2023-02-24 22:41:14 -0300

    Add files via upload

---
+++ PVIII/Aula_2023_02_23_Exer_2/Aula_2023_02_23_Exer_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2023_02_23_Exer_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, total = 0;
+
+            Console.Write("Informe o um valor:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Informe o segundo valor:");
+            b = int.Parse(Console.ReadLine());
+
+            total = a + b;
+
+            Console.Write($"O total é: {total}");
+            Console.ReadLine();
+
+        }
+    }
+}
